9b33be88ad3f967c502532e63ff2b51fafb37de2
Abinash Adhikari <abinashadhikari48@gmail.com>
2024-01-28 23:50:22 +0545

    Abinash: Wrather app first (initial) commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import me from './assets/me.jpg'
 
 function App() {
 
@@ -8,9 +7,11 @@ function App() {
     <>
       <main className='main'>
         <div className="body-content">
-          <h1> Hello, I'm <a className='text-danger' href="http://adhikariabinash.com.np" target="_blank" rel="noopener noreferrer">Er. Abinash Adhikari</a></h1>
-          <img className='my-img' src={me} alt="Abinash Adhikari" />
-          <p className='body-text mt-3'>Welcome to my reaject projects. Just checkout branches and run code. You can customize code as per your requirement. Happy Coding. </p>
+          <h1>Weather Forecast</h1>
+          <section>
+            <p>Weather Forecast code will be here</p>
+          </section>
+          <p className='body-text mt-3'>Welcome to my Weather Forecast projects. You can customize code as per your requirement. Happy Coding. </p>
         </div>
       </main>
     </>
